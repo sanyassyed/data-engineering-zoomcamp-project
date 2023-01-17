98c0fad91fc62533792476d5df4316ac30f4505f
Sanya Syed <sanyashireen@gmail.com>
2023-01-17 11:02:10 -0600

    Dockerfile brought to first test file state

---
+++ week1/2_docker_sql/pipeline.py
@@ -0,0 +1,7 @@
+import sys 
+import pandas 
+
+print('This is a sample python file to learn docker')
+vars = sys.argv
+print(f'The system arguments are as follows {vars}')
+print(f'The file has been executed for the day = {vars[1]}')
